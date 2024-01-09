8b2643682a4b34ea47f32762953f2d40fced8793
Md Raahim Al Amin <45152289+Raahim3108@users.noreply.github.com>
2024-01-09 13:58:03 +0100

    Create MergeTheTools.py
    HackerRank solution

---
+++ MergeTheTools.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    j=0
+    arr = []
+    for i in range(0,len(string),k):
+        arr.append(string[j:j+k])
+        j+=k
+    
+    
+    for i in range(len(arr)):
+        ch = arr[i]
+        for j in range(len(ch)):
+            print(ch[j])
